c3b2460b5fbf64e2a92c14d5afb84dcd7f2247cc
Erica Hashert <ericahashert@gmail.com>
2022-08-01 04:28:00 -0600

    final

---
+++ script.js
@@ -0,0 +1,14 @@
+const input = document.getElementById('friscoLogo');
+input.addEventListener('click', function() {
+    alert("Frisco High School - 6401 Parkwood Blvd, Frisco, TX 75034")
+});
+
+const inputTwo = document.getElementById('obuLogo');
+inputTwo.addEventListener('click', function() {
+    alert(
+        <ul>
+            <li>'Ouachita Baptist University'</li>
+            <li>'410 Ouachita St'</li>
+            <li>'Arkadelphia, AR s71998'</li>
+        </ul>
+    )});
